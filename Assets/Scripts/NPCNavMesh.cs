ec095c5c90da549d32316d2f5fa21a70c3316bb8
Norman <98985926+IGDA-UNO@users.noreply.github.com>
2022-08-26 00:13:24 -0500

    NPCs scripted for movement
    although not quite finished, the NPCNavMesh script has been updated to find a random transform out of 9 possible transforms for each NPC. an NPC is a servant or not. If a servant, it must follow a noblewoman.
    
    the part that isn't fixed is removing "taken" transforms from the list of possible transforms.

--- Assets/Scripts/NPCNavMesh.cs
+++ Assets/Scripts/NPCNavMesh.cs
@@ -5,19 +5,75 @@ using UnityEngine.AI;
 
 public class NPCNavMesh : MonoBehaviour
 {
-    [SerializeField] private Transform movePositionTransform;
+    [SerializeField] private Transform followTransform;
+    public Transform[] viewingTransforms;
+    public Transform[] taken = new Transform[9];
+    public Transform myViewingTransform;
+    public bool spotTaken;
+    public bool isServant;
 
     private NavMeshAgent navMeshAgent;
 
-    private void Awake()
+    void Start()
     {
+        SetViewingTransforms();
+
         navMeshAgent = GetComponent<NavMeshAgent>();
+
+        FindMySpot();   
     }
 
     // Update is called once per frame
     private void Update()
     {
-        navMeshAgent.destination = movePositionTransform.position;
+        if (isServant && followTransform != null)
+        {
+            navMeshAgent.destination = followTransform.position;
+        }
+        else if (!isServant && myViewingTransform != null)
+        {
+            navMeshAgent.destination = myViewingTransform.position;
+        }
+    }
 
+    public Transform RandomizeViewingPositions()
+    {
+        int i = 0;
+        int randIndex = Random.Range(0, viewingTransforms.Length);
+
+        while (taken[i] != null)
+        {
+            if (i < viewingTransforms.Length - 1)
+            {
+                taken[i] = viewingTransforms[randIndex];
+                i++;
+            }
+        }
+
+        return viewingTransforms[randIndex];
+    }
+
+    private void SetViewingTransforms()
+    {
+        viewingTransforms[0] = GameObject.Find("Position (1)").transform;
+        viewingTransforms[1] = GameObject.Find("Position (2)").transform;
+        viewingTransforms[2] = GameObject.Find("Position (3)").transform;
+        viewingTransforms[3] = GameObject.Find("Position (4)").transform;
+        viewingTransforms[4] = GameObject.Find("Position (5)").transform;
+        viewingTransforms[5] = GameObject.Find("Position (6)").transform;
+        viewingTransforms[6] = GameObject.Find("Position (7)").transform;
+        viewingTransforms[7] = GameObject.Find("Position (8)").transform;
+        viewingTransforms[8] = GameObject.Find("Position (9)").transform;
+
+    }
+
+    private void FindMySpot()
+    {
+        if (!spotTaken)
+        {
+            myViewingTransform = RandomizeViewingPositions();
+        }
+
+        spotTaken = true;
     }
 }
