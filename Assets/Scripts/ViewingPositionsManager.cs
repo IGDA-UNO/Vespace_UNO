1538e2776c60b7362170dd7502989adaa44181fd
Norman <98985926+IGDA-UNO@users.noreply.github.com>
2022-08-28 14:43:08 -0500

    ViewingPositionsManager properly updates NPCNavMesh
    Every moving character has there own designated viewing position in the theatre as populated by the ViewingPositionsManager, and received by the NPC's Nav mesh script.

--- Assets/Scripts/ViewingPositionsManager.cs
+++ Assets/Scripts/ViewingPositionsManager.cs
@@ -4,51 +4,24 @@ using UnityEngine;
 
 public class ViewingPositionsManager : MonoBehaviour
 {
-    public Transform[] viewingTransforms;
-    public Transform[] taken = new Transform[9];
-    public NPCNavMesh[] person;
 
-    
+    public List<Transform> viewingTransformsList = new List<Transform>();
+    public List<GameObject> charactersList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        PopulateTaken();
+        PositionAssigner();
     }
 
-    public Transform RandomizeViewingPositions()
+    public void PositionAssigner()
     {
-        
-        int randIndex = Random.Range(0, viewingTransforms.Length - 1);
-       
-        return viewingTransforms[randIndex];
-    }
-
-    public void PopulateTaken()
-    {
-        int i = 0;
-        while (i <= viewingTransforms.Length - 1)
-        {
-           /* if (person[i].SendMySpot() != taken[0] &&
-                person[i].SendMySpot() != taken[1] &&
-                person[i].SendMySpot() != taken[2] &&
-                person[i].SendMySpot() != taken[3] &&
-                person[i].SendMySpot() != taken[4] &&
-                person[i].SendMySpot() != taken[5] &&
-                person[i].SendMySpot() != taken[6] &&
-                person[i].SendMySpot() != taken[7] &&
-                person[i].SendMySpot() != taken[8])
-            {*/
-                taken[i] = person[i].SendMySpot();
-                
-                i++;
-            //}
+        foreach ( GameObject character in charactersList){
+            
+            int currentSpot = Random.Range(0, viewingTransformsList.Count);
+            character.GetComponent<NPCNavMesh>().myViewingTransform = viewingTransformsList[currentSpot];
+            viewingTransformsList.RemoveAt(currentSpot);
         }
-        
     }
+
 }
