c7bb9be6f245eea8e1bc38e9e9747fa52a187be4
Norman <98985926+IGDA-UNO@users.noreply.github.com>
2022-08-28 14:43:08 -0500

    ViewingPositionsManager properly updates NPCNavMesh
    Every moving character has their own designated viewing position in the theatre as populated by the ViewingPositionsManager, and received by the NPC's Nav mesh script.

--- Assets/Scripts/NPCNavMesh.cs
+++ Assets/Scripts/NPCNavMesh.cs
@@ -8,30 +8,20 @@ public class NPCNavMesh : MonoBehaviour
     [SerializeField] private Transform followTransform;
     
     public Transform myViewingTransform;
-    public Transform candidateViewingTransform;
-    public bool spotTaken;
     public bool isServant;
 
     private NavMeshAgent navMeshAgent;
 
-    public ViewingPositionsManager vpm;
+    //public ViewingPositionsManager vpm;
 
     void Start()
     {
-        
         navMeshAgent = GetComponent<NavMeshAgent>();
-        myViewingTransform = null;
-        candidateViewingTransform = null;
-        FindMySpot();
     }
-
-
-
 
     // Update is called once per frame
     private void Update()
     {
-       
         if (isServant && followTransform != null)
         {
             navMeshAgent.destination = followTransform.position;
@@ -40,45 +30,5 @@ public class NPCNavMesh : MonoBehaviour
         {
             navMeshAgent.destination = myViewingTransform.position;
         }
-    }
-
-    private void FindMySpot()
-    {
-       // candidateViewingTransform = vpm.RandomizeViewingPositions();
-
-        while (myViewingTransform == null)
-        {
-            candidateViewingTransform = vpm.RandomizeViewingPositions();
-            if (candidateViewingTransform != vpm.taken[0] &&
-                candidateViewingTransform != vpm.taken[1] &&
-                candidateViewingTransform != vpm.taken[2] &&
-                candidateViewingTransform != vpm.taken[3] &&
-                candidateViewingTransform != vpm.taken[4] &&
-                candidateViewingTransform != vpm.taken[5] &&
-                candidateViewingTransform != vpm.taken[6] &&
-                candidateViewingTransform != vpm.taken[7] &&
-                candidateViewingTransform != vpm.taken[8])
-            {
-                myViewingTransform = candidateViewingTransform;
-            }
-            else
-            {
-                myViewingTransform = null;
-            }
-        }
-    }
-
-    public Transform SendMySpot()
-    {
-        if (myViewingTransform != null)
-        {
-            return myViewingTransform;
-        }
-        else
-        {
-            return null;
-        }
-        
-
     }
 }
