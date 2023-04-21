0859cd5d5da11e6d17f073f0f1cf065761d581b4
Ben Samuel <benjamin.m.samuel@gmail.com>
2023-04-21 18:22:25 -0500

    Character selection window now has official dialogue.

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -37,19 +37,48 @@ public class CharacterSelection : MonoBehaviour
         selectableCharacters = new List<SelectableCharacterHelper>();
         maleNoble = new SelectableCharacterHelper();
         maleNoble.characterName = EnsemblePlayer.MALE_NOBLE;
-        maleNoble.characterDescription = "This character is driven by unique motivations  that are wholly unlike any of the other characters. If you pick this character, you are undoubtedly going to have the single greatest experience of your life.\n\nTruly, I'm envious of you. Would that our places could be swapped, and that *you* were writing the descriptions of these fascinating selectable characters, and *I* was the one charged with the selecting of them.\n\nAlas.";
+        maleNoble.characterDescription = "Your monthly tailor’s bill has gone unpaid for the fourth time, and your banker tells you there’s " +
+                                         "simply no money for your late-night soirées with tout Paris – the fact is, at age 24, you’ve " +
+                                         "squandered generations of family wealth in two wild years at court and in town, and now you " +
+                                         "need some cash.You’ve been tipped off to plans for a theatre that interested investors would " +
+                                        "pay good money for, and despite the indignity you’ll have to go fetch them backstage at " +
+                                        "Bienfait’s marionette theatre. \n\n" +
+                                        "First, give your ticket to the ticket taker. Next, interact with at least two people in the theatre, " +
+                                        "trying to convince them to help you get backstage. Once backstage, get the plans on the other " +
+                                        "side by crossing over the catwalk without being seen by the audience – don’t forget to look at " +
+                                        "the drawings before you put them in your pocket! Once you’re back in the theatre, find the last " +
+                                        "person you spoke to, and make them help you escape!";
         maleNoble.characterImage = maleNobleMaterial;
         maleNoble.titleText = "The Nobleman";
 
         femaleNoble = new SelectableCharacterHelper();
         femaleNoble.characterName = EnsemblePlayer.FEMALE_NOBLE;
-        femaleNoble.characterDescription = "This character is driven by unique motivations  that are wholly unlike any of the other characters. If you pick this character, you are undoubtedly going to have the single greatest experience of your life.\n\nTruly, I'm envious of you. Would that our places could be swapped, and that *you* were writing the descriptions of these fascinating selectable characters, and *I* was the one charged with the selecting of them.\n\nAlas.";
+        femaleNoble.characterDescription = "For weeks you’ve endured the humiliating rumors that your husband, the Marquis, was running " +
+                                            "around with some actress, and then you overheard he’s spending a fortune to have a theatre " +
+                                            "built for the trollop at the Faoire Saint - Germain! Throwing caution to the wind, you decide on a " +
+                                            "course of vengeance and set out to steal the theatre plans at the risk of seeming mad – because " +
+                                            "they’re hidden backstage in, of all places, Bienfait’s marionette theatre! \n\n" +
+                                            "First, give your ticket to the ticket taker. Next, interact with at least two people in the theatre, " +
+                                            "trying to convince them to help you get backstage. Once backstage, get the plans on the other " +
+                                            "side by crossing over the catwalk without being seen by the audience – don’t forget to look at " +
+                                            "the drawings before you put them in your pocket! Once you’re back in the theatre, find the last " +
+                                            "person you spoke to, and try to make as discreet an exit as possible!";
         femaleNoble.characterImage = femaleNobleMaterial;
         femaleNoble.titleText = "The Noblewoman";
 
         servant = new SelectableCharacterHelper();
         servant.characterName = EnsemblePlayer.SERVANT;
-        servant.characterDescription = "This character is driven by unique motivations  that are wholly unlike any of the other characters. If you pick this character, you are undoubtedly going to have the single greatest experience of your life.\n\nTruly, I'm envious of you. Would that our places could be swapped, and that *you* were writing the descriptions of these fascinating selectable characters, and *I* was the one charged with the selecting of them.\n\nAlas.";
+        servant.characterDescription = "You’ve only been in Paris for a few weeks, having come from your native province with a letter " +
+                                        "of recommendation to enter into the service of a powerful noble – and already your master has " +
+                                        "sent you to the Foire Saint-Germain with a strange request. Having invested a large amount of " +
+                                        "money in a proposed theatre at the Fair, he tells you the plans have been stolen and hidden " +
+                                        "somewhere backstage at Bienfait’s marionette theatre by a rival. He’s never really taken notice " +
+                                        "of you before – here’s an opportunity to make an impression!\n\n" +
+                                        "First, give your ticket to the ticket taker. Next, interact with at least two people in the theatre, " +
+                                        "trying to convince them to help you get backstage. Once backstage, get the plans on the other " +
+                                        "side by crossing over the catwalk without being seen by the audience – don’t forget to look at " +
+                                        "the drawings before you put them in your pocket! Once you’re back in the theatre, find the last " +
+                                        "person you spoke to, and try to get out without causing a scandal!";
         servant.characterImage = servantMaterial;
         servant.titleText = "The Servant";
 
